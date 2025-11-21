0051499f92d3bd17cffed5c732746f6f9a8eeeb2
wrdxwrdxwrdx <wrdxwrdxwrdx@gmail.com>
2025-11-21 05:38:53 +0300

    feat: User entity + DTO

---
+++ backend/internal/domain/dto/create_user.go
@@ -0,0 +1,21 @@
+package dto
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type CreateUserRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Username string `json:"username" binding:"required,min=3,max=100"`
+	Password string `json:"password" binding:"required,min=8"`
+}
+
+type CreateUserResponse struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
